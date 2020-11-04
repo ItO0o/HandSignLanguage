af9fff93bef272955502110a2bc2b1b1bfbd68ec
ItO0o <k17014kk@aitech.ac.jp>
2020-11-04 16:41:54 +0900

    手の向きのやつ

---
+++ MotionEditor/Scripts/SwitchSurface.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchSurface : MonoBehaviour
+{
+    [SerializeField]
+    GameObject surface;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSurface() {
+       //if(this.GetComponent<Toggle>().isOn == true){
+       //     surface.transform.position = new Vector3(0,0, 2.2f);
+       // } else {
+       //     surface.transform.position = new Vector3(0, 0, 1.346f);
+       // }
+    }
+}
